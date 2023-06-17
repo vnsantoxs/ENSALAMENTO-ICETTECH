d49c3e016ee94e47ae9751bcc4eed7ef86ba5be6
Vinicius Melchior <viniciusmelchior.vmls@gmail.com>
2023-06-17 14:15:30 -0400

    modified:   Administrador/__pycache__/urls.cpython-310.pyc 	modified:   Administrador/__pycache__/views.cpython-310.pyc 	new file:   Administrador/templates/G_curso.html 	new file:   Administrador/templates/G_disciplina.html 	new file:   "Administrador/templates/G_espa\320\227o-fisico.html" 	new file:   Administrador/templates/G_grade.html 	new file:   Administrador/templates/funcao_Gerenciamento.html 	deleted:    Administrador/templates/menu_Admin.html 	new file:   Administrador/templates/visualizar_Feedback.html 	modified:   Administrador/urls.py 	modified:   Administrador/views.py 	modified:   Aplicacao/__pycache__/urls.cpython-310.pyc 	modified:   Aplicacao/templates/menu_Admin.html 	modified:   Aplicacao/templates/tipocadastro.html 	new file:   static/css/gerenciamento.css 	renamed:    static/css/C_curso.css -> static/css/gerenciarcurso.css 	renamed:    static/css/C_espacofisico.css -> static/css/gerenciardisciplina.css 	renamed:    static/css/C_disciplina.css -> static/css/gerenciarespacofisico.css 	renamed:    static/css/C_grade.css -> static/css/gerenciargrade.css 	modified:   static/css/style_cadastro.css 	new file:   static/css/v_feedback.css

--- Administrador/urls.py
+++ Administrador/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import Menuadministradorview
+from .views import gerennciamento, feedbackview
 
 urlpatterns = [
-    path('menuadministrador/', Menuadministradorview.as_view(), name='menuadministrador'),
+    path('gerenciamento/', gerennciamento.as_view(), name='gerenciamento'),
+    path('visualizarfeedback/', feedbackview.as_view(), name='visualizarfeedback'),
+    path('gerenciamento/curso/', gerennciamento.gerenciamentocurso, name='gerenciamentocurso'),
+    path('gerenrenciamento/disiciplina/', gerennciamento.gerenciamentodisciplina, name='gerenrenciamentodisiciplina'),
+    path('gerenciamento/espacofisico/', gerennciamento.gerenciamentoespacofisico, name='gerenciamentoespacofisico'),
+    path('gerenciamento/grade/', gerennciamento.gerenciamentograde, name='gerenciamentograde'),
 ]
--- Administrador/views.py
+++ Administrador/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-class Menuadministradorview(TemplateView):
-    template_name = 'menu_Admin.html'
+class gerennciamento(TemplateView):
+    template_name = 'funcao_Gerenciamento.html'
+
+    def gerenciamentocurso(request):
+        return render(request,'G_curso.html')
+    
+    def gerenciamentodisciplina(request):
+        return render(request, 'G_disciplina.html')
+    
+    def gerenciamentoespacofisico(request):
+        return render(request, 'G_espaЗo-fisico.html')
+    
+    def gerenciamentograde(request):
+        return render(request, 'G_grade.html')
+    
+class feedbackview(TemplateView):
+    template_name = 'visualizar_Feedback.html'
+    
